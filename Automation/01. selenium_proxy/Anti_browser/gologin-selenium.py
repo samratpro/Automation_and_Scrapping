c9cf07c8c3c575edd135e7106898dfc3ce022ade
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-05-12 01:13:27 +0600

    Update gologin-selenium.py

--- Automation/01. selenium_proxy/Anti_browser/gologin-selenium.py
+++ Automation/01. selenium_proxy/Anti_browser/gologin-selenium.py
@@ -1,16 +1,17 @@
 import time
 from sys import platform
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from gologin import GoLogin
-from gologin import getRandomPort
+from selenium.webdriver.common.by import By
+from time import sleep
 
 
 gl = GoLogin({
-	"token": "yU0token",
-	"profile_id": "yU0Pr0f1leiD"
+	"token": "",
+	"profile_id": ""
 	})
+
 
 if platform == "linux" or platform == "linux2":
 	chrome_driver_path = "./chromedriver"
@@ -18,16 +19,17 @@ elif platform == "darwin":
 	chrome_driver_path = "./mac/chromedriver"
 elif platform == "win32":
 	chrome_driver_path = "chromedriver.exe"
-	
-service = Service(executable_path=chrome_driver_path)
 
 debugger_address = gl.start()
-chrome_options = Options()
+service = Service(executable_path=chrome_driver_path)
+
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("debuggerAddress", debugger_address)
+
 driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-time.sleep(3)
+
+driver.get("link")
+
+sleep(1)
 driver.quit()
-time.sleep(3)
 gl.stop()
